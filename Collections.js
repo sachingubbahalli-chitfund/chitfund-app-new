6dea390c3bc4d4ddc4a67fdd2b39b3371c5a54b2
sachingubbahalli-chitfund <sachingubbahalli@gmail.com>
2026-04-06 04:39:48 +0530

    Add Collections.js component

---
+++ Collections.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Collections = () => {
+  return (
+    <div>
+      <h2>Collections</h2>
+      <p>Monthly contributions and collections will be shown here.</p>
+    </div>
+  );
+};
+
+export default Collections;
