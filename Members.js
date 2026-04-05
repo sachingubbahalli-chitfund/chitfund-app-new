a2cf13909f2483bc03f50238ecf19783c7276099
sachingubbahalli-chitfund <sachingubbahalli@gmail.com>
2026-04-06 04:33:41 +0530

    Add Members.js component

---
+++ Members.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Members = () => {
+  return (
+    <div>
+      <h2>Members</h2>
+      <p>List of all members will appear here.</p>
+    </div>
+  );
+};
+
+export default Members;
