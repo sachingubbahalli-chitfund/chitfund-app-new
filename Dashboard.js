fd9684c1f2507466be4f5321641458ed479f75df
sachingubbahalli-chitfund <sachingubbahalli@gmail.com>
2026-04-06 04:28:56 +0530

    Add Dashboard.js with header

---
+++ Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>ಬಸವೇಶ್ವರ ಕೋ-ಆಪರೇಟಿವ್ ಸೊಸೈಟಿ ಗುಬ್ಬಹಳ್ಳಿ</h1>
+      <p>Welcome to the Chit Fund Dashboard</p>
+    </div>
+  );
+};
+
+export default Dashboard;
