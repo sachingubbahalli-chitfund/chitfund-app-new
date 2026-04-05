e1c8b6360ed314cb5429aba3a18ab9d8f5cb0153
sachingubbahalli-chitfund <sachingubbahalli@gmail.com>
2026-04-06 04:41:36 +0530

    Add Settings.js component

---
+++ Settings.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Settings = () => {
+  return (
+    <div>
+      <h2>Settings</h2>
+      <p>Configure app preferences here.</p>
+    </div>
+  );
+};
+
+export default Settings;
